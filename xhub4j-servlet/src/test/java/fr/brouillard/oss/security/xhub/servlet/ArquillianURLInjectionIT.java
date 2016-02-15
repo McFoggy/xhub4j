6cbdeeb56921b60a57f7db5e0b33ff9953c5a7e6
Matthieu Brouillard <matthieu@brouillard.fr>
2016-02-15 17:54:54 +0100

    add an IT test to demo test URL injection

---
+++ xhub4j-servlet/src/test/java/fr/brouillard/oss/security/xhub/servlet/ArquillianURLInjectionIT.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) 2016 McFoggy [https://github.com/McFoggy/xhub4j] (matthieu@brouillard.fr)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.brouillard.oss.security.xhub.servlet;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+import java.net.URL;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class ArquillianURLInjectionIT {
+    @Deployment(testable = false)
+    public static WebArchive createDeployment() {
+        WebArchive war = ShrinkWrap.create(WebArchive.class).addAsResource(EmptyAsset.INSTANCE, "META-INF/beans.xml");
+        return war;
+    }
+    
+    @ArquillianResource URL deploymentURLAsClassAttribute;
+    
+    @Test
+    public void tested_url_can_be_injected_in_several_ways(@ArquillianResource URL deploymentURLAsParameter) {
+        assertNotNull(deploymentURLAsClassAttribute);
+        assertNotNull(deploymentURLAsParameter);
+        
+        assertThat(deploymentURLAsParameter, is(deploymentURLAsClassAttribute));
+        assertThat(deploymentURLAsParameter.toString(), startsWith("http://"));
+    }
+}
